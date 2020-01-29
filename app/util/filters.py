6e9059852f362c31ad74659c483d417f37b4052e
puffyboa <julian@sankergroup.org>
2020-01-28 20:11:06 -0600

    change report format, added current date

--- app/util/filters.py
+++ app/util/filters.py
@@ -6,7 +6,3 @@ def format_number(num):
     if int(num) == num:
         return int(num)
     return num
-
-# @current_app.context_processor
-# def utility_processor():
-#     return dict(escape=escape)
